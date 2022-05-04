7dcc250ceb5d72a533b206d5b6072b4e6edc5b6a
Mayank Dutta <33067129+mayankdutta@users.noreply.github.com>
2022-05-04 21:23:17 +0530

    Update 6. Merge_sort.cpp

--- 6. Merge_sort.cpp
+++ 6. Merge_sort.cpp
@@ -6,16 +6,16 @@ using namespace std;
 void merge(vector<int> &arr, int l, int mid, int r) {
     vector<int> ans;
     int i = l;
-    int j = mid;
+    int j = mid + 1;
 
-    while (i < mid and j <= r) {
+    while (i <= mid and j <= r) {
         if (arr[i] < arr[j])
             ans.push_back(arr[i++]);
         else
             ans.push_back(arr[j++]);
     }
 
-    while (i < mid) ans.push_back(arr[i++]);
+    while (i <= mid) ans.push_back(arr[i++]);
     while (j <= r) ans.push_back(arr[j++]);
 
     int k = 0;
@@ -25,16 +25,22 @@ void merge(vector<int> &arr, int l, int mid, int r) {
 }
 
 void mergeSort(vector<int> &arr, int l, int r) {
-    if (l < r - 1) {
+    if (l < r) {
         int mid = l + (r - l) / 2;
-        mergeSort(arr, l, mid - 1);
-        mergeSort(arr, mid, r);
+        /*
+         * Don't do, mid - 1, and mid
+         * Just dont
+         * You will get RTE
+         */
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
         merge(arr, l, mid, r);
     }
 }
 
+
 int main() {
-    vector<int> arr{1, 4, 0, 9, 10, INT_MIN, INT_MAX};
+    vector<int> arr{5, 2, 3, 1};
     mergeSort(arr, 0, arr.size() - 1);
     for (const auto &i: arr) {
         cout << i << ' ';
